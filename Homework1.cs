66f2d2e15863acc712fd255007d0d6d5761c359e
mmartinez2-design <mmmartinez2@buffs.wtamu.edu>
2026-01-26 16:43:51 -0600

    uploading HW1

---
+++ Homework1.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main()
+    {
+        double x = 2.5; //setting up x var
+        double y = 3.3; // setting up y var
+        double z = 4*x*x+3*y; //setting up z var
+
+        Console.WriteLine("x = " + x + ",Y =" +y); //displaying x & y values
+        Console.WriteLine("The Value of Z is:" + z); //displaying results of linear equation
+    }
+}
